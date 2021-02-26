51ae0d51b56e0a0732903eefb2a69f08aabc9f45
franciskershaw <franciskershaw2004@hotmail.com>
2021-02-26 14:38:44 +0000

    add: function to change game image depending on user input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
-console.log("helloworld");
+// Change the image on your side of the game depending on which icon you click
+$('#palm').click(function() {
+    $('#player-hand').attr("src", "assets/images/palmfaceup.png") ;
+});
+$('#fist').click(function() {
+    $('#player-hand').attr("src", "assets/images/fistfaceup.png");
+});
