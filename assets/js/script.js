be615369dbdd882e9b08a6c9025d6a5e5c690ac8
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-01 16:26:51 +0000

    complete basic console version of two player game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,22 @@
-
+let userInput;
+let userGuess;
 
 // Event handlers listening for the clicking of both possible user inputs. Also changes the image on your side of the game depending on which icon you click
 $('.hand-icon').click(function() {
     let palm = document.getElementById('palm');
-    let userInput;
     if (this === palm) {
         userInput = 5;
-        console.log(`User input is ${userInput}`);
         $('#player-hand').attr("src", "assets/images/palmfaceup.png");
     } else {
         userInput = 0;
-        console.log(`User input is ${userInput}`);
         $('#player-hand').attr("src", "assets/images/fistfaceup.png");
     }
+    console.log(`User input is ${userInput}`);
 });
 
 $('.number-icon').click(function() {
     let zero = document.getElementById('guess-zero');
     let five = document.getElementById('guess-five');
-    let userGuess;
     if (this === zero) {
         userGuess = 0;
     } else if (this === five) {
@@ -27,33 +25,38 @@ $('.number-icon').click(function() {
         userGuess = 10;
     }
     console.log(`User has guessed ${userGuess}`);
+
+    userTurn () 
 });
 
 // Plays users turn, called once both user inputs have been clicked on.
-function playUserRound () {
-    
+function userTurn () {
+    console.log("UserTurn has been called");
+    // Generate computer's input (either 0 or 5)
+    let computerInput = Math.random() < 0.5 ? 0 : 5;
+    console.log(`Computer has input ${computerInput}`);
+    // Add computer input and user input together to calculate the winning score
+    let correctScore = computerInput + userInput;
+    console.log(`The correct total is ${correctScore}`);
+    // Check if computer's input + user input = userGuess.
+    if (correctScore === userGuess) {
+        console.log('Well done, you. guessed correctly');
+    } else {
+        console.log('Sorry, better luck next time');
+    }
 }
 
-function playComputerRound () {
-
-}
-
-// Computer randomly selects 0 or 5 each round as its input
-function calculateComputerInput() {
+// Code to have computer randomly return input (0 or 5)
+function generateComputerInput() {
     let computerInput = Math.random() < 0.5 ? 0 : 5;
     return computerInput;
 }
 // Help on using the Math.random found from stack overflow post by Peter Olsen at https://stackoverflow.com/questions/9730966/how-to-decide-between-two-numbers-randomly-using-javascript
 
-// Every other round on the computer's go, the computer randomly picks either 0, 5, or 10 as their chosen total score
-function calculateComputerGo () {
+// Code to have computer randomly return guess (0, 5, or 10)
+function generateComputerGo () {
    let computerGo = Math.floor(Math.random() * (2 - 0 + 1) + 0) * 5;
    return computerGo;
-}
-
-// 
-function calculateTotalScore () {
-
 }
 
 // Update the score if the user or computer score matches with the total score
