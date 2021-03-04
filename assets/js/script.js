57f645add7545713613c3b9b30eac513c6f897d9
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-04 17:41:34 +0000

    attempt v2 of game logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,99 +1,52 @@
+// Event listener waiting for the DOM to load
 
+document.addEventListener("DOMContentLoaded", function() {
+    runGame();
+})
 
-
-
-
-// let userInput;
-// let userGuess;
-
-// console.log(`userInput is ${userInput}`);
-// console.log(`userGuess is ${userGuess}`);
-
-// Event handlers listening for the clicking of both possible user inputs. Also changes the image on your side of the game depending on which icon you click
-
-function takeUserInput() {
-    $('.hand-icon').click(function () {
-        if (this === document.getElementById('fist')) {
-            $('#player-hand').attr("src", "assets/images/fistfaceup.png");
-            return 0;
-        } else {
-            $('#player-hand').attr("src", "assets/images/palmfaceup.png");
-            return 5;
-        }
-    });
-}
-
-function takeUserGuess() {
-    $('.number-icon').click(function () {
-        if (this === document.getElementById('zero')) {
-            userGuess = 0;
-        } else if (this === document.getElementById('five')) {
-            userGuess = 5;
-        } else {
-            userGuess = 10;
-        }
-        console.log(`userGuess is ${userGuess}`);
-    });
-}
-
-// Plays users turn, called once 0, 5 or 10 has been clicked.
-
-function userTurn () {
-    
-    let computerInput = generateComputerInput();
-    console.log(`Computer has input ${computerInput}`);
-    if (computerInput === 5) {
-        $('#computer-hand').attr("src", "assets/images/palmfacedown.png");
-    } else {
-        $('#computer-hand').attr("src", "assets/images/fistfacedown.png");
+function runGame() {
+// event listeners for button clicks 
+console.log("runGame has been called");
+let buttons = document.getElementsByTagName("button");
+    for (let button of buttons) {
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "userInput") {
+                alert("you clicked on a hand");
+            } else {
+                alert(`You clicked on a number`);
+            }
+        })
     }
-// Add computer input and user input together to calculate the winning score
-    let correctScore = computerInput + userInput;
-// Update score in the middle
-    $('#round-total').text(`${correctScore}`);
-    console.log(`The correct total is ${correctScore}`);
-// Check if computer's input + user input = userGuess.
-    if (correctScore === userGuess) {
-       incrementPlayerScore();
-        console.log('Well done, you. guessed correctly');
-        computerTurn();
-    } else {
-        incrementComputerScore();
-        console.log("You've guessed incorrectly");
-        computerTurn();
-    }
-    
 }
 
-function computerTurn () {
-    console.log("Time for the computer's go");
-}
+// Randomly generates 0 or 5, as the computer's input each round
+function generateComputerInput () {
+    console.log("generateComputerInput has been called");
+    let computerInput = Math.random() < 0.5 ? 0 : 5;
+    console.log(computerInput);
+};
 
-// Returns 0 or 5 as the computer's input each round
-function generateComputerInput() {
-    return Math.random() < 0.5 ? 0 : 5;
-}
-
-// Update the player score if the user wins a round
-function incrementPlayerScore () {
-    let oldScore = parseInt(document.getElementById("player-score").innerText);
-    document.getElementById("player-score").innerText = ++oldScore;
-}
-
-// Update computer score if the computer wins a round
-function incrementComputerScore () {
-    let oldScore = parseInt(document.getElementById("computer-score").innerText);
-    document.getElementById("computer-score").innerText = ++oldScore;
-}
-
-// // Code to have computer randomly return input (0 or 5)
-// function generateComputerInput() {
-//     return Math.random() < 0.5 ? 0 : 5;
-// }
-// // Help on using the Math.random found from stack overflow post by Peter Olsen at https://stackoverflow.com/questions/9730966/how-to-decide-between-two-numbers-randomly-using-javascript
-
-// // Code to have computer randomly return guess (0, 5, or 10)
 // function generateComputerGuess () {
-//    return Math.floor(Math.random() * (2 - 0 + 1) + 0) * 5;
-// }
+
+// };
+
+// function calculateWinningScore () {
+    
+// };
+
+// function incrementPlayerScore () {
+
+// };
+
+// function incrementComputerScore () {
+
+// };
+
+// function userTurn () {
+
+// };
+
+// function computerTurn ( {
+
+// })
 
