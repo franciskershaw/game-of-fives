8cfe456507a4957bffe1d5e1f57c66d5c54bd650
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-22 16:43:14 +0000

    logic: connect difficulty buttons to index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -7,3 +7,17 @@ $(document).ready(function () {
 window.addEventListener("orientationchange", function() {
     this.console.log('Changed orientation');
 })
+
+
+let difficultyButtons = document.querySelectorAll('.difficulty-button')
+for (let difficultyButton of difficultyButtons) {
+    difficultyButton.addEventListener('click', function() {
+        if (this === document.getElementById('2-player')) {
+            console.log("you clicked 2");
+        } else if (this === document.getElementById('3-player')) {
+            console.log("you clicked 3");
+        } else {
+            console.log("you clicked 4");
+        };
+    });
+};
