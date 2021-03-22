452b7fed78e76980dda0f9f72bb596f72b0ef5db
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-22 17:53:35 +0000

    logic: begin work on 3-player version of the game

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
-    if (window.innerWidth > window.innerHeight === true) {
-        console.log("hello");
+    if (window.innerWidth > window.innerHeight && window.innerWidth < 768) {
+        console.log("Started in landscape on a device less than 1000px");
     }
 })
 
@@ -8,16 +8,24 @@ window.addEventListener("orientationchange", function() {
     this.console.log('Changed orientation');
 })
 
-
 let difficultyButtons = document.querySelectorAll('.difficulty-button')
 for (let difficultyButton of difficultyButtons) {
     difficultyButton.addEventListener('click', function() {
         if (this === document.getElementById('2-player')) {
-            console.log("you clicked 2");
+            game.twoPlayer = true;
+            game.threePlayer = false;
+            game.fourPlayer = false;
+            console.log(`User wants to play two player, game.twoPlayer is ${game.twoPlayer}`);
         } else if (this === document.getElementById('3-player')) {
-            console.log("you clicked 3");
+            game.twoPlayer = false;
+            game.threePlayer = true;
+            game.fourPlayer = false;
+            console.log(`User wants to play three player, game.threePlayer is ${game.threePlayer}`);
         } else {
-            console.log("you clicked 4");
+            game.twoPlayer = false;
+            game.threePlayer = false;
+            game.fourPlayer = true;
+            console.log(`User wants to play four player, game.fourPlayer is ${game.fourPlayer}`);
         };
     });
 };
