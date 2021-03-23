1e65235bc12eeec68a161017a3d5e28ae4dac1db
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-23 15:31:03 +0000

    remove: script tag in index.html, add: explanatory comments in index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,18 +8,17 @@ window.addEventListener("orientationchange", function() {
     this.console.log('Changed orientation');
 })
 
+// Loops over the difficulty buttons in the modal launched once 'Play' button is clicked and uses local storage to assign how many computer players there will be in game object.
+
 let difficultyButtons = document.querySelectorAll('.difficulty-button')
 for (let difficultyButton of difficultyButtons) {
     difficultyButton.addEventListener('click', function() {
         if (this === document.getElementById('2-player')) {
             localStorage.setItem("computerPlayers", 1);
-            console.log(game.computerPlayers);
         } else if (this === document.getElementById('3-player')) {
             localStorage.setItem("computerPlayers", 2);
-            console.log(game.computerPlayers);
         } else {
             localStorage.setItem("computerPlayers", 3);
-            console.log(game.computerPlayers);
         };
     });
 };
