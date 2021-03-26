0e92a6a4a28253ec0b09213f25a949a7f4ebef0e
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-26 16:47:20 +0000

    add: entry animation for elements on index.html

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,31 @@ $(document).ready(function () {
     if (window.innerWidth > window.innerHeight && window.innerWidth < 768) {
         console.log("Started in landscape on a device less than 1000px");
     }
+    entryAnimation();
 })
 
 window.addEventListener("orientationchange", function() {
     this.console.log('Changed orientation');
 })
+
+// Simple animation that introduces the 2 main images followed by the headings
+
+function entryAnimation() {
+    setTimeout(function() {
+        $('.home-images').removeClass('opaque').addClass('home-entry-one');
+    }, 1000);
+    setTimeout(function() {
+        $('.home-images').removeClass('home-entry-one').addClass('home-entry-two');
+    },1500);
+    setTimeout(function() {
+        $('.home-images').removeClass('home-entry-two').addClass('home-entry-three');
+    },2000);
+    setTimeout(function() {
+        $('.home-images').removeClass('home-entry-three');
+        $('.home-headings').removeClass('opaque');
+        $('.home-record').removeClass('opaque');
+    },2500);
+}
 
 // Loops over the difficulty buttons in the modal launched once 'Play' button is clicked and uses local storage to assign how many computer players there will be in game object.
 
