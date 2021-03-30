0529f246e6bd2531c0cec174f8e95f6619bcb8a7
franciskershaw <franciskershaw2004@hotmail.com>
2021-03-30 08:36:29 +0000

    amend: comments on index.js to be consistent

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,11 @@
-// Page loads, checks that the device is in portrait mode (mobile only), updates html depending on users play history and initiates entry animation 
-
+// Once page loads, checks the device is in portrait mode (mobile only), updates html depending on users play history and initiates entry animation 
 $(document).ready(function () {
 	// credit: help with concept and js syntax for checking width and height of window courtesy of Zoe Thexton - https://github.com/zoet24/presidential-debate/blob/master/assets/js/index.js
 	if (window.innerWidth > window.innerHeight && window.innerWidth < 768) {
 		$('.landscape-overlay').removeClass('hidden');
 		$('.home-container').addClass('hidden');
 	}
-	// Sets win vs loss record and amends tagline depending on user's play history
+	// Updates HTML of win vs loss record depending on user's play history
 	let winRecord = localStorage.getItem("winRecord");
 	let loseRecord = localStorage.getItem("loseRecord");
 	let winDifference = winRecord - loseRecord;
@@ -20,7 +19,7 @@ $(document).ready(function () {
 	} else {
 		$('#loss').text(`${loseRecord}`);
 	}
-	// Amends h2 tagline depending on the overall scores
+	// Updates HTML of h2 tagline depending on the overall scores
 	if (winRecord === null && loseRecord === null) {
 		$('#tagline').text('Get stuck into your first game!');
 	} else if (winDifference < 0) {
@@ -33,8 +32,7 @@ $(document).ready(function () {
 	entryAnimation();
 });
 
-// Listens for orientation to hide or remove the warning overlay
-
+// Listens for orientation to show or hide the orientation change warning overlay
 // credit: orientation change event listener help courtesy of mplungjan's post on https://stackoverflow.com/questions/5498934/detect-change-in-orientation-using-javascript
 window.addEventListener("orientationchange", function () {
 	if (window.innerWidth < window.innerHeight && window.innerHeight < 768) {
@@ -51,7 +49,6 @@ window.addEventListener("orientationchange", function () {
 });
 
 // Animation that introduces the 2 main images followed by the heading
-
 function entryAnimation() {
 	setTimeout(function () {
 		$('.home-img').removeClass('opaque').addClass('home-entry-one');
@@ -70,7 +67,7 @@ function entryAnimation() {
 	}, 2500);
 }
 
-// Loops over the difficulty buttons in the play modal and uses local storage to assign how many computer players there will be in game variables object.
+// Loops over the difficulty buttons in the play modal and uses local storage to assign how many computer players there will be in game variables object
 let difficultyButtons = document.querySelectorAll('.difficulty-button');
 for (let difficultyButton of difficultyButtons) {
 	difficultyButton.addEventListener('click', function () {
